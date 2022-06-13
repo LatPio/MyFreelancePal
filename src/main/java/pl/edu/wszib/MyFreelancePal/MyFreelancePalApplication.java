76142e83d41eb79633aff07b0a002898e3c4f631
LatPio <piotr.p.latka@gmail.com>
2022-06-13 14:56:52 +0200

    Initial Commit

---
+++ src/main/java/pl/edu/wszib/MyFreelancePal/MyFreelancePalApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.MyFreelancePal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFreelancePalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFreelancePalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/wszib/MyFreelancePal/MyFreelancePalApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.MyFreelancePal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFreelancePalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
