0f411b5df5b2b148484065f48b3909265dd808cd
LatPio <piotr.p.latka@gmail.com>
2022-07-17 10:58:40 +0200

    Invoice entry version beta problem with adding tasks to OneToMany field

--- src/main/generated/pl/edu/wszib/MyFreelancePal/controller/mapper/InvoiceMapperDTOImpl.java
+++ src/main/generated/pl/edu/wszib/MyFreelancePal/controller/mapper/InvoiceMapperDTOImpl.java
@@ -20,7 +20,7 @@ import pl.edu.wszib.MyFreelancePal.service.domain.ProjectDomain;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-07-17T10:51:38+0200",
+    date = "2022-07-17T10:57:58+0200",
     comments = "version: 1.5.1.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 public class InvoiceMapperDTOImpl implements InvoiceMapperDTO {
--- src/main/generated/pl/edu/wszib/MyFreelancePal/service/mapper/AddressEmployerMapperImpl.java
+++ src/main/generated/pl/edu/wszib/MyFreelancePal/service/mapper/AddressEmployerMapperImpl.java
@@ -10,7 +10,7 @@ import pl.edu.wszib.MyFreelancePal.service.domain.EmployerDomain;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-07-17T10:51:38+0200",
+    date = "2022-07-17T10:57:58+0200",
     comments = "version: 1.5.1.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 public class AddressEmployerMapperImpl implements AddressEmployerMapper {
